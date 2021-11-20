0118185a4580ca7f0d87fb9d62ea644089ca6bc4
kakedasinoyu <nagase.programming@gmail.com>
2021-11-21 00:06:00 +0900

    Add the activity to display edit menu

--- app/src/main/java/com/example/reminder/menu/completed/CompletedDetailActivity.java
+++ app/src/main/java/com/example/reminder/menu/completed/CompletedDetailActivity.java
@@ -1,4 +1,16 @@
 package com.example.reminder.menu.completed;
 
-public class CompletedDetailActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.reminder.R;
+
+public class CompletedDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_completed_detail);
+    }
 }
